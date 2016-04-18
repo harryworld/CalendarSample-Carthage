188d6162d4f0d1ca895f9b92cc4f3ac8a60e31ad
Harry Ng <harryworld@gmail.com>
2016-04-18 23:22:53 +0800

    Create Xcode Project

---
+++ CalendarSample-Carthage/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CalendarSample-Carthage
+//
+//  Created by Harry Ng on 18/4/2016.
+//  Copyright © 2016 STAY REAL LIMITED. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
